9bd8879a54d027edd2794316ab76f33447d01c95
Xian Dong <xiandong1352@gmail.com>
2025-09-28 19:56:19 -0400

    Fixed defensive checks blocking bot from entering VC during first time play

--- commands/music/play.js
+++ commands/music/play.js
@@ -25,10 +25,12 @@ module.exports = {
             .setColor(config.red)
             .setFields({name: "", value: "Please join a vc"});
 
+        const connnection = getVoiceConnection(interaction.guild.id);
+
         if (interaction.member.voice.channelId === null){
             await interaction.reply({embeds: [embed], flags: MessageFlags.Ephemeral});
             return;
-        } else if (botInfo.voice.channelId !== interaction.member.voice.channelId){
+        } else if (botInfo.voice.channelId !== interaction.member.voice.channelId && connnection){
             embed.setFields({name: "", value: "You are not in the same VC channel as the bot"});
             await interaction.reply({embeds: [embed], flags: MessageFlags.Ephemeral});
             return;
@@ -89,7 +91,7 @@ module.exports = {
             if (songList.length > 0){
                 const song = songList[songList.length - 1];
                 embed.setFields({name: "", value: `[${song.title}](${song.songUrl}) **-** \`[${song.duration}]\``}).setColor(config.green)
-                .setAuthor({name: `Added song to queue #${songList.length + 1}`, iconURL: interaction.user.avatarURL()});
+                .setAuthor({name: `Added song to queue #${songList.length}`, iconURL: interaction.user.avatarURL()});
             } else if (songList.length === 0){
                 const song = queue.getCurrentSong();
                 embed.setFields({name: "", value: `[${song.title}](${song.songUrl}) **-** \`[${song.duration}]\``}).setColor(config.green)
