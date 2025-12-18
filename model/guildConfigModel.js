bbfecb4cc243eb01726a39867de8925409bb3ff0
Xian Dong <xiandong1352@gmail.com>
2025-12-18 15:16:01 -0500

    refactor(database) Message logging is now depreciated

--- model/guildConfigModel.js
+++ model/guildConfigModel.js
@@ -3,7 +3,6 @@ const { Schema, model } = require("mongoose");
 const guildSettingsSchema = new Schema (
     {
         guildId: { type: String, required: true, unique: true },
-        messageLogChannel: {type: String, default: null},
         modLogChannel: {type: String, default: null}
     },
     { timestamps: true, versionKey: false }
